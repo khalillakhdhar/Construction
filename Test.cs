2c9a5db92beaaa9ee6c21c4b734f9dfc7909a40e
khalil <khalillakhdharatc@gmail.com>
2022-02-03 10:19:57 +0100

    Initial commit

---
+++ Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Construction.singleton;
+namespace Construction
+{
+    class Test
+    {
+        public void initialize()
+        {
+            Database.creatInstance("localhost");
+        }
+    }
+}
---
+++ singleton/Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construction.singleton
+{
+    public class Database
+    {
+        public static string host;
+
+        private Database()
+        {
+        }
+
+    public static void creatInstance(string hostName)
+        {
+            host = hostName;
+
+        }
+
+
+    }
+}
